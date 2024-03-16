310dbb40cd1f9ec2806e31fc7ccb1270611efc23
demqn <bmkcc@mail.ru>
2024-03-16 20:04:59 +0300

    Task 011

---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/Count.kt
@@ -0,0 +1,18 @@
+package ru.easycode.zerotoheroandroidtdd
+
+interface Count {
+
+    fun increment(number: String): String
+
+    class Base(private val step: Int) : Count {
+
+        init {
+            if (step <= 0) throw IllegalStateException("step should be positive, but was $step")
+        }
+
+        override fun increment(number: String): String {
+            return (step + number.toInt()).toString()
+        }
+    }
+
+}
--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,21 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private val counter = Count.Base(2)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.incrementButton)
+        val count: TextView = findViewById(R.id.countTextView)
+        button.setOnClickListener {
+            count.text = counter.increment(count.text.toString())
+        }
     }
 }

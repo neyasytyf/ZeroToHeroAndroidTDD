541e071a3b4cc0e7f8ed56894064300de3079e41
demqn <bmkcc@mail.ru>
2024-03-19 19:31:29 +0300

    Task 021

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,28 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.widget.addTextChangedListener
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            actionButton.setOnClickListener {
+                titleTextView.text = inputEditText.text
+                inputEditText.text?.clear()
+            }
+            inputEditText.addTextChangedListener {
+                inputEditText.text?.let {
+                    actionButton.isEnabled = it.length > 2
+                }
+            }
+        }
     }
 }
